36356f4b27f179afd3ef33de3ef85662523a7022
Juan Carlos <juank_kenny@hotmail.com>
2013-11-21 09:36:37 -0600

    Initializing repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SSPlatform::Application.config.secret_key_base = '72219b24650d1bb8ca95eaddbf994c47f68129c2c31a6605797e7d7c6c7ac2b3bb8387595fc48a4f6224db1f695eb3c8976093a5b0b7b15cede7ed425809bb43'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SSPlatform::Application.config.session_store :cookie_store, key: '_SS_Platform_session'

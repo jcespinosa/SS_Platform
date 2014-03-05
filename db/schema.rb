7a41ab9947d672806d93e0b66221aef23b2cfcc0
Juan Carlos <juank_kenny@hotmail.com>
2014-03-05 09:53:55 -0600

    Se modifico el esquema de la base de datos

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140106191706) do
+ActiveRecord::Schema.define(version: 20140114203210) do
 
   create_table "project_files", force: true do |t|
     t.string   "name"
@@ -45,13 +45,17 @@ ActiveRecord::Schema.define(version: 20140106191706) do
     t.datetime "updated_at"
     t.string   "password_digest"
     t.string   "remember_token"
-    t.boolean  "admin",           default: false
+    t.boolean  "admin",                   default: false
     t.string   "username"
     t.text     "about"
     t.string   "degree"
     t.string   "job"
     t.string   "workplace"
     t.string   "speciality"
+    t.string   "attachment_file_name"
+    t.string   "attachment_content_type"
+    t.integer  "attachment_file_size"
+    t.datetime "attachment_updated_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
